b827b457623682cd44d8e7b10405db2b117ee109
Patryk Makowski <praca.makowski@gmail.com>
2022-02-01 21:11:31 +0100

    Add guild create automation

--- src/discord.ts
+++ src/discord.ts
@@ -35,6 +35,37 @@ export default (client: Client<boolean>) => {
     console.log('Ready!');
   });
 
+  client.on('guildCreate', (guild) => {
+    const isThereProjectsChannel = guild.channels.cache.some(
+      (channel) => channel.name === 'projects' && channel.type === 'GUILD_TEXT',
+    );
+    if (isThereProjectsChannel) return;
+
+    const isThereprojectManagerRole = guild.roles.cache.some(
+      (role) => role.name === 'project-manager',
+    );
+    if (!isThereprojectManagerRole) {
+      guild.roles.create({ name: 'project-manager', color: 'NAVY' });
+    }
+    const projectManagerRole = guild.roles.cache.find(
+      (role) => role.name === 'project-manager',
+    );
+
+    guild.channels.create('project-manager', {
+      type: 'GUILD_TEXT',
+      permissionOverwrites: [
+        {
+          id: guild.roles.everyone,
+          allow: ['VIEW_CHANNEL'],
+        },
+        {
+          id: projectManagerRole.id,
+          allow: ['SEND_MESSAGES'],
+        },
+      ],
+    });
+  });
+
   client.on('interactionCreate', async (interaction) => {
     if (!interaction.isButton()) return;
 
